6403a829c261f1186f747a11d17e70b5c0b8ee25
Adi <adityaq907@gmail.com>
2024-01-27 15:40:57 +0530

    Added Contact Page

--- JS/menu.js
+++ JS/menu.js
@@ -54,3 +54,11 @@ window.onresize = ()=>{
         }
     }
 }
+
+// smooth scrolling
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top,
+    }, 500, 'linear')
+});
